da5226825340c1be74755d284755e9a13193dd97
Kristian Nygaard Jensen <freeduck@member.fsf.org>
2018-03-22 15:23:33 +0100

    stuff

--- init.el
+++ init.el
@@ -14,7 +14,7 @@
 ;; use-package
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
-(package-install 'use-package))
+  (package-install 'use-package))
 (eval-when-compile
   (require 'use-package))
 
@@ -23,22 +23,72 @@
 (when (fboundp 'tool-bar-mode) (tool-bar-mode 0))
 (when (fboundp 'scroll-bar-mode) (scroll-bar-mode 0))
 
+(defconst kristian-savefile-dir (expand-file-name "savefile" user-emacs-directory))
+
+
+(defun control-media (command)
+  (call-process "smplayer" nil nil nil "-send-action" command))
+
+(defun media-play-pause ()
+  "Play or pause"
+  (interactive)
+  (control-media "play_or_pause"))
+
+(defun media-next ()
+  "Play Next"
+  (interactive)
+  (control-media "play_next"))
+
+(defun media-prev ()
+  "Play Previous"
+  (interactive)
+  (control-media "play_prev"))
+
+(defun media-forward ()
+  "Forward 1 minute"
+  (interactive)
+  (control-media "forward2"))
+
+(defun media-rewind ()
+  "Rewind 1 minute"
+  (interactive)
+  (control-media "rewind2"))
+
+(global-set-key (kbd "C-c m p") 'media-play-pause)
+(global-set-key (kbd "C-c m n") 'media-next)
+(global-set-key (kbd "C-c m P") 'media-prev)
+(global-set-key (kbd "C-c m f") 'media-forward)
+(global-set-key (kbd "C-c m r") 'media-rewind)
+
+(use-package paredit
+  :ensure t
+  :config
+  (add-hook 'emacs-lisp-mode-hook #'paredit-mode)
+  ;; enable in the *scratch* buffer
+  (add-hook 'lisp-interaction-mode-hook #'paredit-mode)
+  (add-hook 'ielm-mode-hook #'paredit-mode)
+  (add-hook 'lisp-mode-hook #'paredit-mode)
+  (add-hook 'eval-expression-minibuffer-setup-hook #'paredit-mode))
+
 (use-package recentf
   :config
-  (setq recentf-save-file (expand-file-name "recentf" bozhidar-savefile-dir)
+  (setq recentf-save-file (expand-file-name "recentf" kristian-savefile-dir)
         recentf-max-saved-items 500
         recentf-max-menu-items 15
         ;; disable recentf-cleanup on Emacs start, because it can cause
         ;; problems with remote files
         recentf-auto-cleanup 'never)
-(recentf-mode +1))
+  (recentf-mode +1))
+
+(use-package cyberpunk-theme
+  :ensure t)
 
 (use-package magit
   :ensure t
   :bind (("C-x g" . magit-status)))
 
 (use-package diminish
-	     :ensure t)
+  :ensure t)
 
 (use-package swiper
   :ensure t
@@ -48,11 +98,11 @@
 
 ;; ivy
 (use-package ivy
-	     :ensure t
-	     :diminish ivy-mode
-	     :config
-	     (ivy-mode 1)
-	     (bind-key "C-c C-r" 'ivy-resume))
+  :ensure t
+  :diminish ivy-mode
+  :config
+  (ivy-mode 1)
+  (bind-key "C-c C-r" 'ivy-resume))
 
 
 (use-package counsel
@@ -69,18 +119,18 @@
   (global-set-key (kbd "C-c j") 'counsel-git-grep)
   (global-set-key (kbd "C-c k") 'counsel-ag)
   (global-set-key (kbd "C-x l") 'counsel-locate)
-(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history))
+  (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history))
 
 
 ;; projectile
 (use-package projectile
-	     :ensure t
-	     :config
-	     (projectile-global-mode)
-	     (setq projectile-mode-line
-		   '(:eval (format " [%s]" (projectile-project-name))))
-	     (setq projectile-remember-window-configs t)
-	     (setq projectile-completion-system 'ivy))
+  :ensure t
+  :config
+  (projectile-global-mode)
+  (setq projectile-mode-line
+	'(:eval (format " [%s]" (projectile-project-name))))
+  (setq projectile-remember-window-configs t)
+  (setq projectile-completion-system 'ivy))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
